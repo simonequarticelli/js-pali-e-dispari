00dac6899a5205dd1188aaa840b2aa2e8bf35e8b
simonequarticelli <quarti87@gmail.com>
2019-04-29 22:15:03 +0200

    console.log(parola[i]) <--- verificare parola al contrario

--- pali/main.js
+++ pali/main.js
@@ -3,30 +3,30 @@
 
 
 //ESERCIZIO CON spit/reverse/join
-//chiedere all'utente parola
-var parola=prompt('inserisci parola', 'parola');
-//controllare se la parola inserita è palindroma
-if (parola.split('').reverse().join('')==parola) {
-  //confermare se lo è o no
-  alert('la parola è palindroma');
-}else {
-  alert('la parola non è palindroma');
-}
+// //chiedere all'utente parola
+// var parola=prompt('inserisci parola', 'parola');
+// //controllare se la parola inserita è palindroma
+// if (parola.split('').reverse().join('')==parola) {
+//   //confermare se lo è o no
+//   alert('la parola è palindroma');
+// }else {
+//   alert('la parola non è palindroma');
+// }
 
 
 //ESERCIZIO CON CICLO FOR
-// //chiedere all'utente parola
-// var parola=prompt('inserisci parola', 'parola');
-// //creo var vuota
-// var str='';
-// //controllo la parola
-// for (var i = parola.length-1; i >= 0; i--) {
-//   console.log(parola);
-//   str+=parola.charAt(i);
-// }
-// //logica
-// if (parola==str) {
-//   alert('la parola è palindroma');
-// } else {
-//   alert('la parola non è palindroma');
-// }
+//chiedere all'utente parola
+var parola=prompt('inserisci parola', 'parola');
+//creo var vuota
+var str='';
+//controllo la parola
+for (var i = parola.length-1; i >= 0; i--) {
+  str+=parola.charAt(i);
+  console.log(parola[i]);
+}
+//logica
+if (parola==str) {
+  alert('la parola è palindroma');
+} else {
+  alert('la parola non è palindroma');
+}

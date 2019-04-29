307f4e422f15f1517f050ac95aeb755d83cbde69
simonequarticelli <quarti87@gmail.com>
2019-04-29 16:41:21 +0200

    inizio progetto js-pali-dispari

---
+++ dispari/main.js
@@ -0,0 +1,7 @@
+// L’utente sceglie pari o dispari e un numero da 1 a 5.
+// Poi generiamo un numero random (sempre da 1 a 5)
+// per il computer e dichiariamo chi ha vinto.
+// (Quindi classico gioco pari o dispari dove utente sceglie a priori se uscirà pari
+// o dipsari e poi dichiara un numero (tra 1 e 5) .
+// Il computer genera random altro numero sullo stesso range,
+// così sapremo chi ha vinto.
---
+++ pali/main.js
@@ -0,0 +1,2 @@
+// Capire se la parola inserita dall’utente è palindroma o no
+// (e quindi output di conseguenza)

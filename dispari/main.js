5bc207ca811ae051f50487c6da3631cca4f3200d
simonequarticelli <quarti87@gmail.com>
2019-04-29 22:10:59 +0200

    fine progetto js-pali-e-dispari

--- dispari/main.js
+++ dispari/main.js
@@ -5,3 +5,23 @@
 // o dipsari e poi dichiara un numero (tra 1 e 5) .
 // Il computer genera random altro numero sullo stesso range,
 // così sapremo chi ha vinto.
+
+
+//scelta utente pari o dispari
+var scelta=prompt('pari o dispari?', 'pari/dispari');
+//scleta numero
+var numero=parseInt(prompt('scegli un numero da 1 a 5', '1/5'));
+//computer
+var cpu=Math.floor(Math.random()*5)+1;
+alert(cpu);
+//somma per capire vincitore
+var somma=numero+cpu;
+//logica
+if (somma%2==0 && scelta=='pari') {  //%2==0 <--- PARI    %2==1<--- DISPARI 
+  //(somma%2==0 && scelta=='pari')||(somma%2==1 && scelta=='dispari') <---con utilizzo OR
+  alert('hai vinto');
+} else if (somma%2==1 && scelta=='dispari'){
+  alert('hai vinto');
+  }else {
+    alert('hai perso');
+}
--- pali/main.js
+++ pali/main.js
@@ -1,2 +1,32 @@
 // Capire se la parola inserita dall’utente è palindroma o no
 // (e quindi output di conseguenza)
+
+
+//ESERCIZIO CON spit/reverse/join
+//chiedere all'utente parola
+var parola=prompt('inserisci parola', 'parola');
+//controllare se la parola inserita è palindroma
+if (parola.split('').reverse().join('')==parola) {
+  //confermare se lo è o no
+  alert('la parola è palindroma');
+}else {
+  alert('la parola non è palindroma');
+}
+
+
+//ESERCIZIO CON CICLO FOR
+// //chiedere all'utente parola
+// var parola=prompt('inserisci parola', 'parola');
+// //creo var vuota
+// var str='';
+// //controllo la parola
+// for (var i = parola.length-1; i >= 0; i--) {
+//   console.log(parola);
+//   str+=parola.charAt(i);
+// }
+// //logica
+// if (parola==str) {
+//   alert('la parola è palindroma');
+// } else {
+//   alert('la parola non è palindroma');
+// }
